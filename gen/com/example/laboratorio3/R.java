54a58c617c1b0190023390fcfa55f308bcd8d85b
luizpalumbo <luizpalumbo@gmail.com>
2015-05-03 14:38:57 -0300

    Atualização de código

--- gen/com/example/laboratorio3/R.java
+++ gen/com/example/laboratorio3/R.java
@@ -25,11 +25,21 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080006;
+        public static final int action_settings=0x7f080010;
         public static final int button1=0x7f080002;
+        public static final int button10=0x7f08000b;
+        public static final int button11=0x7f08000c;
+        public static final int button12=0x7f08000e;
+        public static final int button13=0x7f080005;
+        public static final int button14=0x7f08000f;
         public static final int button2=0x7f080003;
         public static final int button3=0x7f080004;
-        public static final int button4=0x7f080005;
+        public static final int button4=0x7f080006;
+        public static final int button5=0x7f080007;
+        public static final int button6=0x7f080008;
+        public static final int button7=0x7f080009;
+        public static final int button8=0x7f08000d;
+        public static final int button9=0x7f08000a;
         public static final int editText1=0x7f080000;
         public static final int gridLayout1=0x7f080001;
     }
@@ -40,9 +50,22 @@ public final class R {
         public static final int menu_principal=0x7f070000;
     }
     public static final class string {
-        public static final int action_settings=0x7f050002;
+        public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
-        public static final int hello_world=0x7f050001;
+        public static final int btn0=0x7f05000b;
+        public static final int btn1=0x7f050002;
+        public static final int btn2=0x7f050003;
+        public static final int btn3=0x7f050004;
+        public static final int btn4=0x7f050005;
+        public static final int btn5=0x7f050006;
+        public static final int btn6=0x7f050007;
+        public static final int btn7=0x7f050008;
+        public static final int btn8=0x7f050009;
+        public static final int btn9=0x7f05000a;
+        public static final int btnAd=0x7f05000c;
+        public static final int btnDiv=0x7f05000e;
+        public static final int btnMult=0x7f05000f;
+        public static final int btnSub=0x7f05000d;
     }
     public static final class style {
         /** 
